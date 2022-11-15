fbf2a5a89c49ac98ebb7ca55ce50d47081e5d0c4
arjunarj <nagarjunan66@gmail.com>
2022-11-15 23:38:46 +0530

    ...

--- lib/pages/screens/home_page.dart
+++ lib/pages/screens/home_page.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_remix/flutter_remix.dart';
+import 'package:neobank/pages/utils/neobutton.dart';
+
+import 'HomePage/FrontPage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +16,94 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(child: Text("Home Page")));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 10),
+            FrontCard(),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: BorderRadius.circular(50)),
+                        // height: 100,
+                        width: 50,
+                        child: IconButton(
+                          onPressed: () {},
+                          color: Colors.blue,
+                          icon: Icon(FlutterRemix.user_add_line,
+                              color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text("Add accounts")
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: BorderRadius.circular(50)),
+                        // height: 100,
+                        width: 50,
+                        child: IconButton(
+                          onPressed: () {},
+                          color: Colors.blue,
+                          icon: Icon(FlutterRemix.bank_card_line,
+                              color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text("View balance")
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: BorderRadius.circular(50)),
+                        // height: 100,
+                        width: 50,
+                        child: IconButton(
+                          onPressed: () {},
+                          color: Colors.blue,
+                          icon: Icon(FlutterRemix.money_dollar_circle_line,
+                              color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text("Buy Assets")
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
